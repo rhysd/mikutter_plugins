ea91ff17b08afc74a8bdba15fbfd886f74e26eb1
rhysd <lin90162@yahoo.co.jp>
2012-06-09 02:28:13 +0900

    ツイート内のツイートURLをツイート本文に展開する機能．とりあえず書いただけでまだテストしていない．スクリーン名をマッチング側から取ってくることで少し高速化も追加．

--- expand_tweet_url.rb
+++ expand_tweet_url.rb
@@ -1,1 +1,22 @@
-#TODO
+autoload :Twitter, 'twitter'
+
+Plugin.create :filter do
+
+    filter_show_filter do |msgs|
+        msg.each do |m|
+            m[:message].gsub! /https?:\/\/twitter\.com\/([_a-zA-Z0-9]+?)\/(\d+)/ do
+                if $1 && $2
+                    begin
+                        status = Twitter.status $2.to_i
+                        "(refer to: @" + $1 + ": " + status["text"] + " )"
+                    rescue
+                        "https://twitter.com/" + $1 + "/" + $2
+                    end
+                else
+                    "(refer to: 参照先を取得できませんでした )"
+                end
+            end
+        end
+    end
+
+end
