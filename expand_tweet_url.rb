22a3963f69d2897bb1e50f343525443e7c6bceb9
rhysd <lin90162@yahoo.co.jp>
2012-06-12 15:07:05 +0900

    戻り値を修正．

--- expand_tweet_url.rb
+++ expand_tweet_url.rb
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# TODO: t.co を展開しないと使えない
 Plugin.create :filter do
 
     filter_show_filter do |msgs|
-        msg.each do |m|
-            m[:message].gsub! /https?:\/\/twitter\.com\/([_a-zA-Z0-9]+?)\/status\/(\d+)/ do
+        msgs.each do |m|
+            m[:message] = m[:message].gsub /https?:\/\/twitter\.com\/([_a-zA-Z0-9]+?)\/status\/(\d+)/ do
                 if $1 && $2
                     require 'twitter'
                     begin
@@ -12,10 +15,11 @@ Plugin.create :filter do
                         "https://twitter.com/" + $1 + "/status/" + $2
                     end
                 else
-                    "(refer to: 参照先を取得できませんでした )"
+                    '(refer to: 参照先を取得できませんでした )'
                 end
             end
         end
+        [msgs]
     end
 
 end
